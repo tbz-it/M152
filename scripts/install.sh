37c6a43f4c738bbe3777ad04b14c44c76851acec
Thomas Kälin <kaelin@sweb.ch>
2020-04-30 00:32:36 +0200

    apt-get statt apt

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,8 @@
 #	Installationsscript Modul 152 - Multimedia-Inhalte in Webauftritt integrieren
 
 # Install apache, php, ftp, powershell, markdown to HTML
-sudo apt update
-sudo apt install -y apache2 php libapache2-mod-php vsftpd markdown
+sudo apt-get update
+sudo apt-get install -y apache2 php libapache2-mod-php vsftpd markdown
 sudo snap install powershell --classic
 
 # Introseite (= README dieses Repository)
